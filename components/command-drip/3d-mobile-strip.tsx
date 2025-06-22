dcd24f0ed7ed769c58618c35e39d3b06f0cb4de0
suraj-xd <unofficialartist3@gmail.com>
2025-06-22 13:44:35 +0530

    s

---
+++ components/command-drip/3d-mobile-strip.tsx
@@ -0,0 +1,8 @@
+export default function ThreeDMobileStrip() {
+  return (
+    // <div className="absolute top-0 left-0 flex md:hidden w-full h-full justify-center items-center">
+    //   <h1 className="text-sm text-center">For best experience, please use a desktop browser</h1>
+    // </div>
+    null
+  );
+}
